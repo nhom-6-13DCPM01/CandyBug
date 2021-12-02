86b38e378846852e244a79f2a69f9e8bbb3084ad
PhanNhutTruong <tuong22223@gmail.com>
2021-12-02 21:59:56 +0700

    Phân vùng Folder Admin and Client

--- CandyBug/App_Start/RouteConfig.cs
+++ CandyBug/App_Start/RouteConfig.cs
@@ -14,10 +14,12 @@ namespace CandyBug
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                 name: "Default",
+                 url: "{controller}/{action}/{id}",
+                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                 new[] { "CandyBug.Controllers" }
+             );
+           
         }
     }
 }
--- CandyBug/Areas/Admin/Controllers/HomeController.cs
+++ CandyBug/Areas/Admin/Controllers/HomeController.cs
@@ -4,17 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace CandyBug.Controllers
+namespace CandyBug.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Admin/Home
         public ActionResult Index()
         {
-
-            //Set Route đến vùng Admin
-            /*return View("~/Areas/Admin/Views/Home/Index.cshtml");*/
             return View();
         }
-
     }
 }
