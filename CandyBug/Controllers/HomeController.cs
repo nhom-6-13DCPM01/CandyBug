12fa89bfdb74d95f91fa67e24649a1352d594ab1
PhanNhutTruong <tuong22223@gmail.com>
2021-12-06 14:20:06 +0700

    Model Complete

--- CandyBug/Controllers/HomeController.cs
+++ CandyBug/Controllers/HomeController.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using CandyBug.Models;
 namespace CandyBug.Controllers
 {
     public class HomeController : Controller
     {
+        CandybugOnlineEntities db = new CandybugOnlineEntities();
         public ActionResult Index()
         {
-
+            
             //Set Route đến vùng Admin
             /*return View("~/Areas/Admin/Views/Home/Index.cshtml");*/
             return View();
         }
 
+        public ActionResult ListProduct()
+        {
+            var list = db.Products.ToList();
+            return View(list);
+        }
     }
 }
