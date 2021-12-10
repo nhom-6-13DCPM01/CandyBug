ea6bc1c18bbd429eacf1588b75a8e810e2633a50
PhanNhutTruong <tuong22223@gmail.com>
2021-12-10 14:51:31 +0700

    Function Login & Register Complete

--- CandyBug/Areas/Admin/Controllers/HomeController.cs
+++ CandyBug/Areas/Admin/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CandyBug.Models;
 
 namespace CandyBug.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+       
         // GET: Admin/Home
         public ActionResult Index()
         {
