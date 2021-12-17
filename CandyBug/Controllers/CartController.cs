19640e835a851abe7f397d9f67da29d76b47688e
PhanNhutTruong <tuong22223@gmail.com>
2021-12-17 14:07:33 +0700

    Đã tải pagelist- ajax- cập nhật entity

---
+++ CandyBug/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CandyBug.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            ViewBag.ActiveCart = "active";
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            ViewBag.ActiveCheckout= "active";
+            return View();
+        }
+    }
+}
