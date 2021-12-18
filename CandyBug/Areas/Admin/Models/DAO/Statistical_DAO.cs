0aa2c3c52656d4d1943c48ae69d9900560dea8bb
thinhtruong2305 <56267717+thinhtruong2305@users.noreply.github.com>
2021-12-18 17:02:07 +0700

    Initial Statistical_DAO commit

---
+++ CandyBug/Areas/Admin/Models/DAO/Statistical_DAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CandyBug.Models;
+
+namespace CandyBug.Areas.Admin.Models.DAO
+{
+    public class Statistical_DAO
+    {
+        CandybugOnlineEntities DBCandyBug;
+        
+        public Statistical_DAO() { DBCandyBug = new CandybugOnlineEntities(); }
+
+
+    }
+}
