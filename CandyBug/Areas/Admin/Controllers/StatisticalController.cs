4b078a6bbb43fdbaa97a5deb7b5feec0f28bec39
thinhtruong2305 <56267717+thinhtruong2305@users.noreply.github.com>
2021-12-20 19:21:11 +0700

    Hoàn tất thống kê commit
    Có tạo thêm một folder model trong areas/admin trong folder đó có 2 folder DAO và EF, DAO là kết nối với DataBase và EF là đối tượng
    Có thể xem chi tiết bằng cách bấm vào detail và chọn ngày bằng cách bấm vào lọc theo ngày
    Lưu ý: Lọc theo ngày không thể bắt lỗi null nên hạn chế bấm khi chưa đặt dữ liệu

---
+++ CandyBug/Areas/Admin/Controllers/StatisticalController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CandyBug.Areas.Admin.Model.DAO;
+using CandyBug.Areas.Admin.Model.EF;
+using CandyBug.Models;
+using System.Web.Routing;
+
+namespace CandyBug.Areas.Admin.Controllers
+{
+    public class StatisticalController : Controller
+    {
+        private Statistical_DAO statistical = new Statistical_DAO();
+        private CandybugOnlineEntities DBCandyBug = new CandybugOnlineEntities();
+
+        // GET: Admin/Statistical
+        public ActionResult Index()
+        {
+            return View(statistical.getDanhSachThongKe());
+        }
+
+        [HttpGet, ActionName("Detail")]
+        //Hiển thị thông tin chi tiết về hóa đơn
+        public ActionResult Detail(int id)
+        {
+            var hoaDon = DBCandyBug.Oders.Find(id);
+            ViewBag.HoaDon = new Statistical
+            {
+                maHoaDon = hoaDon.Id,
+                ngayTao = hoaDon.DateCreate,
+                trangThai = hoaDon.Status
+            };
+            return View(statistical.getThongTinHoaDon(id));
+        }
+
+        public ActionResult LocTheoNgay()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("LocTheoNgay")]
+        public ActionResult LocTheoNgay(DateTime fromDate, DateTime toDate)
+        {
+            List<Statistical> danhSach;
+            if ((fromDate.Date > toDate.Date))
+            {
+                ViewBag.ThongBaoLoiDuLieuDauVao = "Vui lòng chọn lại ngày!";
+                return View();
+            }
+            else
+            {
+                danhSach = statistical.getDanhSachThongKeByDate(fromDate, toDate);
+            }
+            return View("ThongKeByDate", danhSach);
+        }
+
+        [ActionName("ThongKeByDate")]
+        public ActionResult ThongKeByDate(List<Statistical> danhSach)
+        {
+            return View(danhSach);
+        }
+    }
+}
---
+++ CandyBug/Areas/Admin/Model/DAO/Statistical_DAO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CandyBug.Models;
+using CandyBug.Areas.Admin.Model.EF;
+
+namespace CandyBug.Areas.Admin.Model.DAO
+{
+    public class Statistical_DAO
+    {
+        CandybugOnlineEntities DBCandyBug = new CandybugOnlineEntities();
+
+        public List<Statistical> getDanhSachThongKe()
+        {
+            var danhSach = (from u in DBCandyBug.Oders
+                            where u.Status.Equals("GIAO HÀNG THÀNH CÔNG")
+                            select new Statistical
+                            {
+                                maHoaDon = u.Id,
+                                ngayTao = u.DateCreate,
+                                tongTien = u.OrderInfoes.Select(c => c.Total).Sum(),
+                                trangThai = u.Status,
+                            }).ToList();
+            return danhSach;
+        }
+
+        public List<Statistical> getDanhSachThongKeByDate(DateTime fromDate, DateTime toDate)
+        {
+            var danhSach = (from u in DBCandyBug.Oders
+                            where u.DateCreate >= fromDate && u.DateCreate <= toDate
+                            where u.Status.Equals("GIAO HÀNG THÀNH CÔNG")
+                            select new Statistical
+                            {
+                                maHoaDon = u.Id,
+                                ngayTao = u.DateCreate,
+                                tongTien = u.OrderInfoes.Select(c => c.Total).Sum(),
+                                trangThai = u.Status,
+                            }).ToList();
+            return danhSach;
+        }
+
+        public List<HoaDon> getThongTinHoaDon(int idOrder)
+        {
+            var danhSach = (from u in DBCandyBug.OrderInfoes
+                            where u.IdOrder == idOrder
+                            select new HoaDon
+                            {
+                                maHoaDon = u.IdOrder,
+                                tenSanPham = u.Product.Name,
+                                gia = u.Product.Price,
+                                soLuong = u.Quantity,
+                                tongTien = u.Total
+                            }).ToList();
+            return danhSach;
+        }
+    }
+}
